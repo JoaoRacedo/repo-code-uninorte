21163387b1ca1e90ca2c842f6c778fd734dc0fa9
Sebastian <sebasgarcep@gmail.com>
2017-11-20 12:16:46 -0500

    fix: weird issues

--- app/Controllers/Http/CredentialController.js
+++ app/Controllers/Http/CredentialController.js
@@ -1,10 +1,15 @@
 'use strict'
 
+const CredentialModel = use('App/Models/Credential')
+const UserModel = use('App/Models/User')
+
 class CredentialController {
   async signInWithEmailAndPassword ({ request, auth }) {
     const { email, password } = request.post()
     const data = await auth.attempt(email, password)
-    return { data }
+    const credential = await CredentialModel.findBy('email', email)
+    const user = await UserModel.find(credential.user_id)
+    return { data, user }
   }
 }
 
